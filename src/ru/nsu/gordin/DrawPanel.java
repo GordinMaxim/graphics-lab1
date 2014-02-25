af03b0e77089d4289b2f54b22ee2d9f159c46612
gormakc <gormakc.ru@gmail.com>
2014-02-25 23:37:48 +0700

    change circle drawing

--- src/ru/nsu/gordin/DrawPanel.java
+++ src/ru/nsu/gordin/DrawPanel.java
@@ -2,22 +2,24 @@ package ru.nsu.gordin;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.Line2D;
+import java.awt.image.BufferedImage;
 
 public class DrawPanel extends JPanel {
 
     public void paintComponent(Graphics g) {
         super.paintComponent(g);
         Graphics2D g2 = (Graphics2D) g;
+        BufferedImage image = new BufferedImage(300, 300, BufferedImage.TYPE_3BYTE_BGR);
 
-        double x0 = 200, y0 = 100;
-        double radius = 50;
-        double x = 0, y = 0;
+        int x0 = 150, y0 = 150;
+        int radius = 50;
+        int x = 0, y = 0;
         for(double angle = 0; angle < 2 * Math.PI; angle += Math.toRadians(1))
         {
-            x = radius * Math.cos(angle);
-            y = radius * Math.sin(angle);
-            g2.draw(new Line2D.Double(x0 + x, y0 + y, x0 + x, y0 + y));
+            x = (int)(radius * Math.cos(angle)) + x0;
+            y = (int)(radius * Math.sin(angle)) + y0;
+            image.setRGB(x, y, Color.WHITE.getRGB());
         }
+        g2.drawImage(image, 0, 0, null);
     }
 }
